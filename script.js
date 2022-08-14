2d8f99e3a524c5d2428c342297e234a0e19e3804
EgorovZaxar555 <101442263+EgorovZaxar555@users.noreply.github.com>
2022-08-14 23:47:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  var regex = /[0-9]|\./;
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
+function insert(num) {
+  document.form.textview.value = document.form.textview.value + num;
+}
+function clean() {
+  document.form.textview.value = "";
+}
+function back() {
+  var exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length - 1);
+}
+function equal() {
+  var exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
+}
